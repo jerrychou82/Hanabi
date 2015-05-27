537e6e4edc68e72fe815ace7ee306d308a4d161c
andyyuan <iamyuanchung@gmail.com>
2015-05-27 16:41:17 +0800

    add if __name__ == '__main__':

--- socket_practice/client.py
+++ socket_practice/client.py
@@ -1,14 +1,16 @@
 import socket
 import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("linux6.csie.ntu.edu.tw", int(sys.argv[1])))
 
-msg = sock.recv(1024)
-judge_port = int(msg)
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("linux6.csie.ntu.edu.tw", int(sys.argv[1])))
 
-print "judge port " + str(judge_port)
-jsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-jsock.connect(("linux6.csie.ntu.edu.tw", judge_port))
-msg = jsock.recv(1024)
-print msg
+    msg = sock.recv(1024)
+    judge_port = int(msg)
+
+    print "judge port " + str(judge_port)
+    jsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    jsock.connect(("linux6.csie.ntu.edu.tw", judge_port))
+    msg = jsock.recv(1024)
+    print msg
