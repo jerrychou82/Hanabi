9a1e943f913bf970d2afdd222f8b8f528ebb0ea7
Jerry Chou <b01902046@linux1.csie.ntu.edu.tw>
2015-06-03 21:29:47 +0800

    server: croom, groom

--- server.py
+++ server.py
@@ -12,32 +12,25 @@ Execution:
 # port number
 port_num = int(sys.argv[1])
 
-# maximum number of clients
-MAX_PLAYER  = 20
-ROOM_NUM    = 20
 
 # connection socket
 conn_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 conn_sock.bind((socket.gethostname(), port_num))
-conn_sock.listen(MAX_PLAYER)
-
-# user list
-user_list   = []
-room_list   = []
-judge_list  = []
+conn_sock.listen(Server.MAX_PLAYER)
 
 # reading queue
 rqueue      = [conn_sock]
-# select timeou
+
+# server objct
+server  = Server(rqueue)
+
+# select timeout
 timeout     = 1
 
-# rooms init
-for i in xrange(ROOM_NUM):
-    room = Room(rID=i)
 
 while 1:
     
-    rlist, wlist, elist = select.select(rqueue,[], [], timeout)
+    rlist, wlist, elist = select.select(server.rqueue,[], [], timeout)
 
     for s in rlist:
         if s == conn_sock: # new connection
@@ -45,30 +38,38 @@ while 1:
             csock, addr = conn_sock.accept()
             csock.send(("ACK").encode('UTF-8'))
             print("Send ACK")
-            rqueue.append(csock)        # client socket
+            server.rqueue.append(csock)        # client socket
             user = User(usock=csock)           
-            user_list.append(user)      # user
+            server.user_list.append(user)      # user
             if user.usock == csock:
                 print("Success")
 
         else:
             print("User")
-            for user in user_list:
+            uflag = 0;
+            for user in server.user_list:
+                if uflag == 1:
+                    break
                 if s == user.usock: # user send message
+                    # server.user_msg(s, user)
+                    uflag = 1;
                     print("Matched")
                     data = s.recv(4096)
                     if not data: # user leave
                         print(user.uname + " leaves")
-                        user_list.remove(user)
-                        rqueue.remove(user.usock)
+                        server.user_list.remove(user)
+                        server.rqueue.remove(user.usock)
                     msg = data.decode('UTF-8')
                     msg_list = msg.split(' ')
+                    
+                    # select service
                     if msg_list[0] == "login":
                         user.uname = msg_list[1]
                         s.send(("login ACK").encode('UTF-8'))
                         print("New user: " + msg_list[1])
+                    
                     elif msg_list[0] == "update":
-                        info = make_lobby_info(user_list, room_list)
+                        info = make_lobby_info(server.user_list, server.room_list)
                         info = "update " + info
                         s.send(info.encode('UTF-8'))
                         print("Send: " + info)
@@ -80,29 +81,30 @@ while 1:
                         else:
                             rid     = int(msg_list[1])
                             max_num = int(msg_list[2])
-                            if rid < 0 or rid >= ROOM_NUM or \
-                                    room_list[rid].status != "EMPTY":
+                            print("rid: " + str(rid) + " max_num: " + str(max_num))
+                            if rid < 0 or rid >= Server.ROOM_NUM or server.room_list[rid].rstatus != "EMPTY":
                                 s.send(("croom DENY").encode('UTF-8'))
                                 print("Create room deny: wrong room number")
                             else:
-                                room = room_list[rid]
-                                user_list           = []
-                                user_status         = []
-                                user_list.append(user)
-                                user_status.append("WAIT")
-                                room.user_list      = user_list
-                                room.user_status    = user_status
+                                room = server.room_list[rid]
+                                room_user_list           = []
+                                room_user_status         = []
+                                room_user_list.append(user)
+                                room_user_status.append("WAIT")
+                                room.user_list      = room_user_list
+                                room.user_status    = room_user_status
                                 room.max_unum       = max_num
                                 room.status         = "WAIT"
                                 s.send(("croom ACK").encode('UTF-8'))
                                 print("Create room")
+                    
                     elif msg_list[0] == "groom": # groom [room num]
                         rid = int(msg_list[1])
-                        if rid < 0 or rid >= ROOM_NUM:
+                        if rid < 0 or rid >= Server.ROOM_NUM:
                             s.send(("groom DENY".encode('UTF-8')))
                             print("Go to room deny")
                         else:
-                            room = romm_list[rid]
+                            room = server.romm_list[rid]
                             if len(room.user_list) < room.max_unum:
                                 room.user_list.append(user)
                                 room.user_status("WAIT")
@@ -111,9 +113,29 @@ while 1:
                                 s.send(("groom DENY".encode('UTF-8')))
                                 print("Go to room deny: room full")
 
-                else:
+                    elif msg_list[0] == "ready":
+                        rid = int(msg_list)
+                        if rid < 0 or rid >= Server.ROOM_NUM:
+                            s.send("ready DENY".encode('UTF-8'))
+                            print("ready error: wrong room id")
+                        else:
+                            s.send("ready ACK".encode('UTF-8'))
+                            rflag = -1
+                            for i in xrange(len(server.room_list[rid].user_list)):
+                                if server.room_list[rid].user_list[i].usock == s:
+                                    rflag = i
+                                    break
+                            if rflag == -1:
+                                s.send("ready DENY".encode('UTF-8'))
+                                print("ready error: not in room")
+                            else:
+                                server.room_list[rid].user_status[rflag] = "READY"
+                                s.send("ready ACK".encode('UTF-8'))
+                                print(user.uname + " ready!")
+
+                if uflag == 0:
                     print("Non matched")
-                    data = user.recv(4096)
+                    data = s.recv(4096)
                     print(data)
 
 
--- server_kernel.py
+++ server_kernel.py
@@ -1,5 +1,30 @@
 from game_control import *
 
+class Server:
+        
+    # maximum number of clients
+    MAX_PLAYER  = 20
+    ROOM_NUM    = 20
+
+    def __init__(self, rqueue): 
+    
+        # list
+        self.user_list  = []
+        self.room_list  = []
+        self.judge_list = []
+        
+        # rooms init
+        for i in xrange(Server.ROOM_NUM):
+            room = Room(rID=i)
+            self.room_list.append(room)
+        
+        # reading queue
+        self.rqueue          = rqueue
+
+    def user_msg(self, s):
+        
+
+
 def make_lobby_info(user_list, room_list):
     
     info = ""
