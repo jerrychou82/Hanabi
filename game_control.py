c81a4f29986564abd3ba61373383074acbb7e7f8
Jerry Chou <b01902046@linux1.csie.ntu.edu.tw>
2015-06-03 14:42:42 +0800

    recover game control

---
+++ game_control.py
@@ -0,0 +1,107 @@
+"""
+This module defines several classes that will be used in game system.
+
+"""
+import socket
+
+
+class User:
+
+    def __init__(self, ustatus="CONN",
+                 uname=None, uID=None, uIP=None, usock=None,
+                 roomID=None, gameID=None):
+        """
+        :type ustatus: string
+        :param ustatus: IDLE, ROOM, GAME, CONN
+
+        :type uname: string
+        :param uname: name of the user
+
+        :type uID: int
+        :param userID: ID of the user
+
+        :type uIP: string
+        :param userIP: IPv4 address of the user
+
+        :type usock: socket object
+        :param usock: socket of the user
+
+        :type roomID: int
+        :param roomID: ID of the room where the user in
+
+        :type gameID: int
+        :param gameID: ID of the game which the user is involved
+
+        """
+        self.ustatus = ustatus
+        self.uname = uname
+        self.uID = uID
+        self.uIP = uIP
+        self.usock = usock
+        self.roomID = roomID
+        self.gameID = gameID
+
+
+class Room:
+
+    def __init__(self, rID=None, rstatus="WAIT",
+                 user_list=[], user_status=[], max_unum=4):
+        """
+        :type rID: int
+        :param rID: ID of the room
+
+        :type rstatus: string
+        :param rstatus: WAIT, PLAY
+
+        :type user_list: list
+        :param user_list: list of User objects
+
+        :type user_status: list (of strings)
+        :param user_status: list of ths status of all users in this room
+
+        :type max_unum: int
+        :param max_unum: maximum number of users in this room
+
+        """
+        self.rID = rID
+        self.rstatus = rstatus
+        self.user_list = user_list
+        self.user_status = user_status
+        self.max_unum = max_unum
+
+
+class Judge:
+
+    def __init__(self, jID=None,
+                 room_in_charge=None,
+                 game_info=None,
+                 port=None):
+        """
+        :type jID: int
+        :param jID: ID of the judge
+
+        :type room_in_charge: Room object
+        :param room_in_charge: room the judge is currently in charge of
+
+        :type game_info:
+        :param game_info:
+
+        :type port: int
+        :param port: port number of the judge
+
+        """
+        self.jID = judgeID
+        self.room_in_charge = room_in_charge
+        # TODO: don't understand what game_info is ...
+        self.game_info = None
+        self.port = port
+
+
+def main():
+    user = User(ustatus="CONN",
+                uname="andyyuan", uID=16, uIP="140.112.90.192",
+                roomID=1, gameID=1)
+
+
+if __name__ == '__main__':
+    main()
