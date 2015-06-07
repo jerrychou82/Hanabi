673d8b826ab25af0e07b32550d103981fdc7d47c
jerrychou82 <jerrychou82@gmail.com>
2015-06-07 12:50:03 +0800

    game.py: tabe issue

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import random
 class Game:
 
     def __init__(self, player_num=0, desk=[], hint=None, fail=None, players=[], hint_list=[], garbage=[], hanabi=[]):
-		self.player_num = player_num
+        self.player_num = player_num
         self.desk = desk
         self.hint = hint
         self.fail = fail
@@ -17,35 +17,35 @@ class Game:
     def serve(self, player, cards):
         self.players[player] = Game_player(cards)
 
-	def hit(self, player, cardidx, card):
-		self.players[player].update_card(cardidx], card)
+    def hit(self, player, cardidx, card):
+            self.players[player].update_card(cardidx], card)
 
-	def throw(self, player, cardidx, card):
-		self.players[player].update_card(cardidx], card)
+    def throw(self, player, cardidx, card):
+            self.players[player].update_card(cardidx], card)
 
-	def add_hint(self, sender, recver, hinttype, cards):
-		res = Hint(sender, recver, hinttype, cards)
-		self.hint_list.append(res)
+    def add_hint(self, sender, recver, hinttype, cards):
+            res = Hint(sender, recver, hinttype, cards)
+            self.hint_list.append(res)
 
-	def show_garbage(self):
-		print ('Garbage ', end='')
-		for i in range(len(self.garbage)):
-			print (garbage[i], end='')
-		print ('')
+    def show_garbage(self):
+            print ('Garbage ', end='')
+            for i in range(len(self.garbage)):
+                    print (garbage[i], end='')
+            print ('')
 
-	def show_hanabi(self):
-	    print ('Hanabi ' + str(hanabi))
-	
-	def show_hint_list(self):
-		print ('Hint_list ', end='')
-		for i in range(len(self.hint_list)):
-			print (str(hint_list), end='')
-		print ('')
+    def show_hanabi(self):
+        print ('Hanabi ' + str(hanabi))
+    
+    def show_hint_list(self):
+            print ('Hint_list ', end='')
+            for i in range(len(self.hint_list)):
+                    print (str(hint_list), end='')
+            print ('')
 
-	def show_status(self):
-		print ('Hint %d Fail %d' % (self.hint, self.fail))
-		self.show_garbage()
-		self.show_hanabi()
+    def show_status(self):
+            print ('Hint %d Fail %d' % (self.hint, self.fail))
+            self.show_garbage()
+            self.show_hanabi()
 
 
 class Game_player:
@@ -60,10 +60,10 @@ class Game_player:
 class Hint:
 
     def __init__(self, sender=None, recver=None, hinttype=None, cards=[]):
-		self.sender = sender
-		self.recver = recver
-		self.hinttype = hinttype
-		self.cards = cards
+        self.sender = sender
+        self.recver = recver
+        self.hinttype = hinttype
+        self.cards = cards
 
     def show_hint(self):
         mtype = 'undefined'
